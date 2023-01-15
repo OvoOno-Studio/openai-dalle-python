b3ebba500398f6db1237853bca76a35ea077f9cd
unknown <ebu.pro@gmail.com>
2023-01-15 21:29:42 +0100

    typo

--- garbage/forms copy.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import TextField
-from wtforms.validators import DataRequired, Length
-
-# Set your Form classes below:
-class GenerateForm(FlaskForm):
-    text = TextField('Text', validators=[DataRequired(), Length(min=2, max=40)])
-    recaptcha = RecaptchaField()
