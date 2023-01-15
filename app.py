0c68022d75a5e0d0d56e14d638e5416c9c678a69
unknown <ebu.pro@gmail.com>
2023-01-15 21:28:28 +0100

    typo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 from flask import Flask, request, redirect, url_for, render_template, jsonify
 # from flask.ext.sqlalchemy import SQLAlchemy
 from config import APIKey, InfuraKey, dbPW 
-from forms.forms import *
+from forms import GenerateForm
 from web3 import Web3
 import openai 
 import os
