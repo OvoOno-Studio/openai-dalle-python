39e56c0c571d5d97f1f94ec3f059659e99f345e7
unknown <ebu.pro@gmail.com>
2023-01-15 19:13:30 +0100

    typo

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, 
+from wtforms.validators import DataRequired, EqualTo, Length
 
 # Set your Form classes below:
 class GenerateForm(FlaskForm):
