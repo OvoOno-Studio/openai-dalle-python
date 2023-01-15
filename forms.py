5da6ba4e17205a15af148193a841c340babe05fb
unknown <ebu.pro@gmail.com>
2023-01-15 19:48:19 +0100

    fix forms.py

--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
-from flask_wtf import Form
-from wtforms import TextField
-from wtforms.validators import DataRequired, Length, StringField, DecimalField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import TextField, StringField, DecimalField, SubmitField
+from wtforms.validators import DataRequired, Length, Regexp
 
 # Set your Form classes below:
-class GenerateForm(Form):
-    name = TextField(
+class GenerateForm(FlaskForm):
+    text = TextField(
         'Text', validators=[DataRequired(), Length(min=2, max=40)]
     )
 
-class DonateForm(Form):
+class DonateForm(FlaskForm):
     wallet_address = StringField('Wallet Address', validators=[DataRequired(), Length(min=42, max=42), Regexp('^0x[a-fA-F0-9]{40}$', message='Enter a valid Ethereum address')])
     amount = DecimalField('Amount (Ether)', validators=[DataRequired()])
     submit = SubmitField('Donate')
