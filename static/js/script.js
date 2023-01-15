203ad406f66c5a50c32c5d791511018ed03c68ac
unknown <ebu.pro@gmail.com>
2023-01-16 00:01:38 +0100

    test

--- static/js/script.js
+++ static/js/script.js
@@ -1,61 +1,63 @@
-const form = document.getElementById('form');
-console.log('works');
-function onSubmit(){ 
-    event.preventDefault();        
-    console.log('works also');  
-    // Execute the reCAPTCHA verification
-    grecaptcha.ready(function() {
-        grecaptcha.execute('6LdeTdAjAAAAAPGWzX9fKu4lUXMeef2zaAhs-nXy', { action: 'submit' }).then(function(token) {
-            // Add the reCAPTCHA token to the request header
-            const text = form.elements['text'].value;
-            const imageContainer = document.getElementById('image-container');
-            const linkContainer = document.getElementById('link-container');
+(function() {
+    const form = document.getElementById('form');
+    console.log('works');
+    function onSubmit(){ 
+        event.preventDefault();        
+        console.log('works also');  
+        // Execute the reCAPTCHA verification
+        grecaptcha.ready(function() {
+            grecaptcha.execute('6LdeTdAjAAAAAPGWzX9fKu4lUXMeef2zaAhs-nXy', { action: 'submit' }).then(function(token) {
+                // Add the reCAPTCHA token to the request header
+                const text = form.elements['text'].value;
+                const imageContainer = document.getElementById('image-container');
+                const linkContainer = document.getElementById('link-container');
 
-            document.getElementById('loader').style.display = 'block';
-            // Remove all child nodes of the image container
-            if (imageContainer.firstChild) {
-                while (imageContainer.firstChild) {
-                    imageContainer.removeChild(imageContainer.firstChild);
-                }
-            }
-
-            if (linkContainer.firstChild) {
-                while (linkContainer.firstChild) {
-                    linkContainer.removeChild(linkContainer.firstChild);
-                }
-            }
-
-            fetch('/generate', {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'text/plain',
-                'x-recaptcha-token': token,
-                },
-                body: text,
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Request failed with status code: ${response.status}`);
+                document.getElementById('loader').style.display = 'block';
+                // Remove all child nodes of the image container
+                if (imageContainer.firstChild) {
+                    while (imageContainer.firstChild) {
+                        imageContainer.removeChild(imageContainer.firstChild);
+                    }
                 }
 
-                return response.json();
-            })
-            .then(data => {
-                const image = document.createElement('img');
-                const url = document.createElement('a');
-                image.src = data.url;
-                url.href = data.url;
-                url.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" /></svg><span> Download Image </span>';
-                url.target = '_blank';
-                imageContainer.appendChild(image);
-                linkContainer.appendChild(url);
-                document.getElementById('loader').style.display = 'none';
-            })
-            .catch(error => {
-                console.error(error);
-                document.getElementById('loader').style.display = 'none';
+                if (linkContainer.firstChild) {
+                    while (linkContainer.firstChild) {
+                        linkContainer.removeChild(linkContainer.firstChild);
+                    }
+                }
+
+                fetch('/generate', {
+                    method: 'POST',
+                    headers: {
+                    'Content-Type': 'text/plain',
+                    'x-recaptcha-token': token,
+                    },
+                    body: text,
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Request failed with status code: ${response.status}`);
+                    }
+
+                    return response.json();
+                })
+                .then(data => {
+                    const image = document.createElement('img');
+                    const url = document.createElement('a');
+                    image.src = data.url;
+                    url.href = data.url;
+                    url.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24"><path d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" /></svg><span> Download Image </span>';
+                    url.target = '_blank';
+                    imageContainer.appendChild(image);
+                    linkContainer.appendChild(url);
+                    document.getElementById('loader').style.display = 'none';
+                })
+                .catch(error => {
+                    console.error(error);
+                    document.getElementById('loader').style.display = 'none';
+                });
             });
         });
-    });
-};
-window.onSubmit = onSubmit; 
+    };
+    window.onSubmit = onSubmit; 
+}).call(this);
