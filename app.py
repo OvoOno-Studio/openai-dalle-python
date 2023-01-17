91ff0dde3f7b6d790f5fa8465d694680c370e661
unknown <ebu.pro@gmail.com>
2023-01-17 04:34:12 +0100

    remove unused data

--- app.py
+++ app.py
@@ -3,21 +3,16 @@
 #----------------------------------------------------------------------------#
 from flask import Flask, request, redirect, url_for, render_template, jsonify 
 # from flask.ext.sqlalchemy import SQLAlchemy
-from config import APIKey, InfuraKey, dbPW, SecretKey 
-from forms.forms import *
-from web3 import Web3
-import openai   
-import qrcode 
+from config import APIKey, dbPW, SecretKey 
+from forms.forms import * 
+import openai    
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 # Counts per IP
-request_counts = {}
-
-# Connect to the Ethereum network using Infura
-web3 = Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{InfuraKey}"))
+request_counts = {} 
 
 # Set the API key
 openai.api_key = str(APIKey)
@@ -29,11 +24,7 @@ def index():
     form = GenerateForm(request.form)
     donate_form = DonateForm(request.form)
     return render_template('pages/placeholder.home.html', form=form, donate_form=donate_form) 
-
-@app.route('/donate', methods=['POST'])
-def donate():
-    form = DonateForm() 
-
+ 
 @app.route('/generate', methods=['POST', 'GET'])
 def generate():
     # Get the text to generate an image for
